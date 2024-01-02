6f40374ab8e494b602c1d06947b95e6fab4ec72c
HuangLab-Fudan <46698453+HuangLab-Fudan@users.noreply.github.com>
2024-01-02 15:09:18 +0800

    Add files via upload

---
+++ Library_Construction_parallel1.py
@@ -0,0 +1,66 @@
+import re
+import threading
+import subprocess
+import argparse
+import multiprocessing
+import util
+
+#x_front,front library construction
+x_front={}
+def library_construction_front(pep,num):
+	save=[]
+	for i in util.modules:
+		if pep!=i:
+			seq=i+pep
+			antigen_list=util.SB_antigen(seq,num,util.e1,util.e2,util.e3)
+			no_need=util.find_element(antigen_list,util.modules)
+			if len(no_need)==0:
+				save.append(i)
+	x_front[pep]=save
+#x,end library construction
+x={}
+def library_construction_end(pep,num):
+	save=[]
+	for i in util.modules:
+		if pep!=i:
+			seq=pep+i
+			antigen_list=util.SB_antigen(seq,num,util.e1,util.e2,util.e3)
+			no_need=util.find_element(antigen_list,util.modules)
+			if len(no_need)==0:
+				save.append(i)
+	x[pep]=save
+
+#distribute threads,suitable for less kernels of cpu
+def process_data_threaded_front(m):
+	d={}
+	for i in range(len(m)):
+		d["t"+str(i)]=threading.Thread(target=library_construction_front,args=(m[i],i))
+	for i in d.keys():
+		d[i].start()
+	for i in d.keys():
+		d[i].join()
+def process_data_threaded_end(m):
+	d={}
+	for i in range(len(m)):
+		d["t"+str(i)]=threading.Thread(target=library_construction_end,args=(m[i],i))
+	for i in d.keys():
+		d[i].start()
+	for i in d.keys():
+		d[i].join()
+#process threads for front library construction
+process_data_threaded_front(util.modules)
+
+#front_sorted_length，sort the front library
+length = {}
+for k in x_front.keys():
+	length[k]=len(x_front[k])
+front_sorted_length = dict(sorted(length.items(), key=lambda x: x[1]))
+
+#process threads for end library construction
+process_data_threaded_end(util.modules)
+
+#sorted_length，sort for the end library
+length={}
+for k in x.keys():
+	length[k]=len(x[k])
+sorted_length = dict(sorted(length.items(), key=lambda x: x[1]))
---
+++ add_linker.py
@@ -0,0 +1,48 @@
+#add_linker
+import re
+import threading
+import subprocess
+import argparse
+import multiprocessing
+from gor4 import GOR4
+import util
+import optimal_path_filtering
+x_front=optimal_path_filtering.x_front
+x=optimal_path_filtering.x
+front_sorted_length=optimal_path_filtering.front_sorted_length
+sorted_length=optimal_path_filtering.sorted_length
+sequence=optimal_path_filtering.current_sequence
+new_antigen_list = util.find_element(util.SB_antigen(optimal_path_filtering.current_sequence,0,util.e1,util.e2,util.e3), util.modules)
+linker_library = ["GGGGSGGGGS","SGGGGSGGGG","GSGSGSGSGS","GGSGGSGGSGGS","GGGGSGGGGSAAA","GGGGSGAAAGGSGGGG","GSGSSGSGSS"]
+result_linker = {}
+if len(new_antigen_list)==0:
+        final=sequence
+        print("result")
+        print(final)
+else:
+        for i in linker_library:
+                A = util.in_B(new_antigen_list,util.modules)
+                x = util.check_position(A,util.modules)
+                y = util.check_index(A,sequence)
+                new_sequence = util.find_and_replace(A,x,y,i,sequence)
+                if len(util.find_element(util.SB_antigen(new_sequence,0,util.e1,util.e2,util.e3),util.modules))==0:
+                        final = new_sequence
+                        result_linker[i]=new_sequence
+                        print("linker",i)
+                        print(final)
+filename=str(util.p).split("/")
+filename_2=filename[len(filename)-1]
+filename_3=filename_2.split(".")[0]+"_result.txt"
+with open(filename_3,"w") as f:
+        if result_linker:
+            f.write("linker")
+            f.write("\t")
+            f.write("sequence")
+            f.write("\n")
+            for i in result_linker.keys():
+                    f.write(i)
+                    f.write("\t")
+                    f.write(result_linker[i])
+                    f.write("\n")
+        else:
+            f.write(final)
